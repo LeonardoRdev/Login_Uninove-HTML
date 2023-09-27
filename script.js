3668705f3f9674a252564e9c24575d9441d69d3b
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-09-27 11:36:44 -0300

    missão adicionada

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function validar(user, password) {
-    let mensagem = document.querySelector("main p#mensagem_main")
+function validar() {
+    let user = document.querySelector("form input#user").value;
+    let password = document.querySelector("form input#password").value;
+    let mensagem = document.querySelector("main p#mensagem_main");
     if (user == "admin" && password=="uninove") {
-        mensagem.innerHTML = "Acesso CONCEDIDO"
-        mensagem.style.color = "#2af76b"
+        mensagem.innerHTML = "Acesso CONCEDIDO";
+        mensagem.style.color = "#2af76b";
         mensagem.style.display = "flex";
         setTimeout(() => window.open("https://aluno.uninove.br/seu/CENTRAL/aluno/"), 500) /*espera 0.5 segundos antes de abrir a página*/
     } else {
@@ -18,4 +20,12 @@ function lembrar() {
     mensagem.innerHTML = "Usuário: admin <br> Senha: uninove";
     mensagem.style.color = "#f1f1f1";
     mensagem.style.display = "flex";
+}
+
+function missao() {
+    let mensagem = document.querySelector("main p#mensagem_main");
+    mensagem.innerHTML = "Missão:<br> A missão da Uninove é ensinar e capacitar todos os alunos para que sejam aptos a entrar no mercado de trabalho";
+    mensagem.style.display = "flex";
+    mensagem.style.color = "#f1f1f1";
+    mensagem.style.textDecoration = "none";
 }
