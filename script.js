1187866cb471ba2bcdb518ceedbddab792792c14
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-09-27 13:46:15 -0300

    Adicionado função cursos

--- script.js
+++ script.js
@@ -30,7 +30,10 @@ function lembrar() {
 
 function missao() {
     let mensagem = document.querySelector("main p#mensagem_main");
-    mensagem.innerHTML = "Missão:<br> A missão da Uninove é ensinar e capacitar todos os alunos para que sejam aptos a entrar no mercado de trabalho";
+    mensagem.innerHTML = "Missão:<p id='descricao_missao'> Nossa missão é ensinar e capacitar todos os alunos para que sejam aptos a entrar no mercado de trabalho.</p>";
+    let mensagemMissao = document.querySelector("main p#mensagem_main p#descricao_missao");
+    mensagemMissao.style.display = "flex";
+    mensagemMissao.style.fontSize = "2rem";
     mensagem.style.display = "flex";
     mensagem.style.color = "#f1f1f1";
     mensagem.style.textDecoration = "none";
@@ -39,4 +42,15 @@ function missao() {
 function limpar_mensagem() {
     let mensagem = document.querySelector("main p#mensagem_main");
     mensagem.style.display = "none";
+}
+
+function curso() {
+    let mensagem = document.querySelector("main p#mensagem_main");
+    mensagem.innerHTML = "Cursos:<p id='descricao_cursos'> A Uninove oferece diversos cursos de alta qualidade em diversas áreas do conhecimento, preparando os alunos para o mercado de trabalho e valorizando a inovação e o empreendedorismo.</p>";
+    let mensagemCurso = document.querySelector("main p#mensagem_main p#descricao_cursos");
+    mensagemCurso.style.display = "flex";
+    mensagemCurso.style.fontSize = "2rem";
+    mensagem.style.color = "white";
+    mensagem.style.textDecoration = "none";
+    mensagem.style.display = "flex";
 }
